d917d326058233dd55ea241304127e2834ee9398
seungwook0417 <swlee@daegu.ac.kr>
2022-11-05 15:30:22 +0000

    queue 추가

--- routers/router.py
+++ routers/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 import routers.container.container as container
 import routers.image.image as image
+import routers.vulnerability.scan as scan
 
 api_router = APIRouter()
 
 api_router.include_router(container.router, prefix='/container', tags=['container'])
 api_router.include_router(image.router, prefix='/image', tags=['image'])
+api_router.include_router(scan.router, prefix='/scan', tags=['scan'])
