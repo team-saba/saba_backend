42f326dd9be0109f4bf427d814362321e04a3ba4
Ch1keen <40013212+Ch1keen@users.noreply.github.com>
2022-11-21 23:25:01 +0900

    Ch1keen feature clair integration (#46)
    * refactor: Seperate clair parts
    
    * fix: clair_post_manifest had 401 errors
    
    * fix: GET /scan/{image_id} had an 500 error
    
    * refactor: error handlign
    
    * feat: 스캔할 때 엔진을 Trivy 혹은 Clair로 선택하여 사용할 수 있음
    
    * fix: static 폴더를 찾지 못해 에러 메시지가 뜨던 문제 수정

--- worker.py
+++ worker.py
@@ -9,7 +9,7 @@ import diskcache
 from urllib3.exceptions import HTTPError
 from DTO.VulnerabilityQueue import VulnerabilityQueue
 import service.image_service as manage
-
+import service.clair_service as clair
 
 class ReservationWorker:
     request_count = 0
@@ -20,11 +20,7 @@ class ReservationWorker:
     reservation_success_list = []
     async_tasks = set()
 
-    auth_url = 'https://auth.docker.io/token'
-    docker_registry_url = 'https://registry-1.docker.io/'
-    docker_manifest_url = 'https://registry-1.docker.io/v2/{}/manifests/{}'
     clair_indexer_url = 'http://localhost:6060/indexer/api/v1/index_report'
-    clair_matcher_url = 'http://localhost:6060/matcher/api/v1/vulnerability_report/'
 
     def __init__(self):
         self.worker_status = diskcache.Cache(directory="./cache/worker_status")
@@ -84,44 +80,45 @@ class ReservationWorker:
                 image_id=reservation.imageId
             )
 
-            # Trivy Data Saving
-            reservation.result = []
-            trivy_result = reservation_ticket['scan_result']
-            for t in trivy_result:
-                vid = t['VulnerabilityID']
-                d   = t['Description']
-                s   = t['Severity']
-                p   = t['PkgName']
-                pi  = t['InstalledVersion']
-                pix = t.get('FixedVersion', '')
+            if reservation.scan_on_trivy:
+                # Trivy Data Saving
+                reservation.result = []
+                trivy_result = reservation_ticket['scan_result']
+                for t in trivy_result:
+                    vid = t['VulnerabilityID']
+                    d   = t['Description']
+                    s   = t['Severity']
+                    p   = t['PkgName']
+                    pi  = t['InstalledVersion']
+                    pix = t.get('FixedVersion', '')
 
-                reservation.result.append({
-                    'cveid': vid,
-                    'description': d,
-                    'severity': s,
-                    'packageName': p,
-                    'packageInstalled': pi,
-                    'packageFixedIn': pix,
-                    'engine': 'trivy',
-                })
+                    reservation.result.append({
+                        'cveid': vid,
+                        'description': d,
+                        'severity': s,
+                        'packageName': p,
+                        'packageInstalled': pi,
+                        'packageFixedIn': pix,
+                        'engine': 'trivy',
+                    })
 
 
-            # Clair Second
-            if len(reservation.digest) == 0:
-                # TODO: local image scan
-                # named container scan maybe?
-                raise Exception("Local image scan is currently not supported.")
+            if reservation.scan_on_clair:
+                # Clair Second
+                if len(reservation.digest) == 0:
+                    # TODO: local image scan
+                    raise Exception("Local image scan is currently not supported.")
 
-            digest = self.validate_digest(reservation)
-            response = requests.get(self.clair_indexer_url + f'/{digest}')
-            if response.status_code == 404:
-                layers = self.clair_get_layers(reservation)
-                self.clair_post_manifest(layers=layers, reservation=reservation)
-            elif response.status_code == 500:
-                parsed_response = json.loads(response.content)
-                raise HTTPError(parsed_response)
+                digest = clair.validate_digest(reservation)
+                response = requests.get(self.clair_indexer_url + f'/{digest}')
+                if response.status_code == 404:
+                    layers = clair.clair_get_layers(reservation)
+                    clair.clair_post_manifest(layers=layers, reservation=reservation)
+                elif response.status_code == 500:
+                    parsed_response = json.loads(response.content)
+                    raise HTTPError(parsed_response)
 
-            clair_result = self.clair_get_report(digest)
+            clair_result = clair.clair_get_report(digest)
 
             if clair_result != {}:
                 for ck, cv in clair_result['vulnerabilities'].items():
@@ -184,122 +181,6 @@ class ReservationWorker:
             self.write_worker_status()
             await asyncio.sleep(delay)
 
-    # Private Functions
-    def clair_post_manifest(self, layers: list, reservation: VulnerabilityQueue):
-        token = self.get_auth_token(reservation)
-        header = { 'Authorization': [ f'Bearer {token}' ] }
-        digest = reservation.digest
-        repo = reservation.repo_name
-        _layers = []
-        for layer in layers:
-            layer_digest = layer['digest']
-            layer_uri = self.docker_registry_url
-            layer_uri += f'/v2/{repo}/blobs/{layer_digest}'
-
-            _layers.append({'hash': layer_digest, 'uri': layer_uri, 'headers': header})
-
-        _manifest = {
-                        'hash': digest,
-                        'layers': _layers
-                    }
-
-        response = requests.post(self.clair_indexer_url, json=_manifest)
-        return response.status_code
-
-    def clair_get_layers(self, reservation: VulnerabilityQueue):
-        token = 'Bearer ' + self.get_auth_token(reservation)
-        header = {'Authorization': token}
-        digest = reservation.digest
-        repo = reservation.repo_name
-        if repo.find('/') == -1:
-            repo = 'library/' + repo
-
-
-        request_url = self.docker_manifest_url.format(repo, digest)
-        response = requests.get(request_url, headers=header)
-        parsed_res = json.loads(response.content)
-
-        if 'layers' in parsed_res:
-            return parsed_res['layers']
-
-        elif 'manifests' in parsed_res:
-            # It must have 'layers', not 'manifests'
-            # see 'validate_digest' function
-            raise Exception('Cannot get manifest: digest not validated.\n' + parsed_res)
-
-        else:
-            raise Exception('Cannot get manifest: Unexpected Response.\n' + parsed_res)
-
-    def validate_digest(self, reservation: VulnerabilityQueue):
-        repo = reservation.repo_name
-        if repo.find('/') == -1:
-            repo = 'library/' + repo
-        digest = reservation.digest
-        token = 'Bearer ' + self.get_auth_token(reservation)
-        header = {'Authorization': token}
-
-        request_url = self.docker_manifest_url.format(repo, digest)
-        response = requests.get(request_url, headers=header)
-        if response.status_code == 401:
-            raise HTTPError(response.content)
-
-        parsed_res = json.loads(response.content)
-        if 'manifests' in parsed_res:
-            for manifest in parsed_res['manifests']:
-                # For this reason, arm is not supported currently.
-                if manifest['platform']['architecture'] == 'amd64':
-                    reservation.digest = manifest['digest']
-                    return reservation.digest
-
-        elif 'layers' in parsed_res:
-            return reservation.digest
-
-        else:
-            raise Exception('Cannot validate digest: Unexpected response' + parsed_res)
-
-
-    def clair_get_report(self, digest: str):
-        response = requests.get(self.clair_matcher_url + digest)
-        if response.status_code == 401:
-            error_report = 'clair_get_report: 401 Unauthorized\n'
-            error_report += json.loads(response.content)
-            raise HTTPError(error_report)
-        if response.status_code >= 500:
-            error_report = 'clair_get_report: Database or clair server has error; it might be because it is busy.'
-            error_report += json.loads(response.content)
-            raise HTTPError(error_report)
-        return json.loads(response.content)['vulnerabilities']
-
-    def get_auth_token(self, rsv: VulnerabilityQueue)->str:
-        # request for token of 'ch1keen/pwnable': OK
-        # request for token of 'library/ruby': OK
-        # request for token of 'ruby': OK but won't work
-        repository = rsv.repo_name
-        if repository.find('/') == -1:
-            repository = 'library/' + repository
-
-        token = self.auth_token.get(repository)
-        if token is None:
-            params = {
-                        'service': 'registry.docker.io',
-                        'scope': f'repository:{repository}:pull',
-                     }
-            response = requests.get(self.auth_url, params = params)
-
-            if response.status_code == 200:
-                token_json = json.loads(response.content)
-
-                self.auth_token.set(repository, token_json['token'], expire=token_json['expires_in'])
-                return token_json['token']
-
-            else:
-                logging.error("Cannot retrieve a token: Received %d" % response.status_code)
-                logging.error(json.loads(response.content))
-
-                raise HTTPError('get_auth_token: Cannot retrieve the token.')
-
-        else:
-            return str(token)
 
 async def main():
     tasks = set()
