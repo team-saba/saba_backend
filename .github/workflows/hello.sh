810fa122a75cc394ccd03a20bfa0edf833aeeab7
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-11-24 02:16:52 +0900

    test

--- .github/workflows/hello.sh
+++ .github/workflows/hello.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-if [ $# -eq 1 ]; then
-  imageId=$1
-  echo "[image Id] ${imageId}\n"
-
-# 1. scan/start_reservation_process
-# 2. scan/result
-# 3. save result of scan/result, check critical vulnerability count of scanResult
-# 4. siging image according to diagnosis
-# 5. return result of each step
-
+imageId=$1
+echo "[image Id] ${imageId}\n"
