cbef840b0cd4ad8baed8743e5dc0209ce4210ace
seungwook0417 <swlee@daegu.ac.kr>
2022-11-26 07:07:29 +0000

    test

--- service/test.py
+++ service/test.py
@@ -4,7 +4,8 @@ import image_id as imageId
 
 def test():
     imageId =imageId.image_id
-    result = service.scan_image(imageId)
-    print(result)
+    print(imageId)
+    # result = service.scan_image(imageId)
+    # print(result)
 
 test()
