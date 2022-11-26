6dd6e44f08bc5c5de0adda5d1d12b40e4b7e71c4
seungwook0417 <swlee@daegu.ac.kr>
2022-11-26 07:35:59 +0000

    test

--- service/test.py
+++ service/test.py
@@ -1,16 +1,12 @@
 import image_service as service
-import image_id as iamgeFile 
 import sys
 
-print(sys.argv[0])
-print(sys.argv[1])
 
 
-def test():
-    imageId =iamgeFile.image_id
-    result = service.scan_image(imageId)
-    
-    print(imageId)
-    print(result)
+# imageId =sys.argv[1]
+# result = service.scan_image(imageId)
 
-test()
+# print(imageId)
+# print(result)
+
+print(service.print_list())
