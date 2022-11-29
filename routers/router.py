5e19bdb6bde681ecbd45269d98e1c82256ae0c1a
seungwook0417 <swlee@daegu.ac.kr>
2022-11-29 19:28:40 +0000

    [feat] user settings slack oauth, auto scan setting

--- routers/router.py
+++ routers/router.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 import routers.container.container as container
 import routers.image.image as image
 import routers.vulnerability.scan as scan
-import routers.slack.slack as slack
+import routers.setting.setting as slack
 
 api_router = APIRouter()
 
 api_router.include_router(container.router, prefix='/container', tags=['container'])
 api_router.include_router(image.router, prefix='/image', tags=['image'])
 api_router.include_router(scan.router, prefix='/scan', tags=['scan'])
-api_router.include_router(slack.router, prefix='/slack', tags=['slack'])
+api_router.include_router(slack.router, prefix='/setting', tags=['setting'])
---
+++ routers/setting/setting.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+
+from DTO.userSetting import UserSetting
+from service.settingManager import SettingManager
+
+router = APIRouter()
+
+
+
+@router.post("/oauth")
+def oauth(code: str):
+    return SettingManager().slack_oauth(code)
+
+
+@router.get("/oauth")
+def oauth(code: str):
+    return SettingManager().slack_oauth(code)
+
+@router.get("/settings")
+def get_settings():
+    return SettingManager().get_setting()
+
+
+@arouter.put("/settings")
+def put_settings(user_setting: UserSetting):
+    return SettingManager().update_setting(user_setting=user_setting)
